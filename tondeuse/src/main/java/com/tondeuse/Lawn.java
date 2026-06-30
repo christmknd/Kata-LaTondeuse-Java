e5131f81233def77b1d2c3279e1babe7808fcd2d
christmknd <christmokonda@gmail.com>
2026-06-30 12:19:34 +0200

    ADD getters & setters on Position & Lawn

--- tondeuse/src/main/java/com/tondeuse/Lawn.java
+++ tondeuse/src/main/java/com/tondeuse/Lawn.java
@@ -2,12 +2,31 @@ package com.tondeuse;
 
 public class Lawn {
 
-    int maxX;
-    int maxY;
+    private int maxX;
+    private int maxY;
 
     Lawn(int maxX, int maxY) {
         this.maxX = maxX;
         this.maxY = maxY;
+    }
+
+    // getters
+    public int getMaxX() {
+        return maxX;
+
+    }
+
+    public int getMaxY() {
+        return maxY;
+    }
+
+    // setters
+    public int setMaxX(int maxX) {
+        this.maxX = maxX;
+    }
+
+    public int setMaxY( int maxY) {
+        this.maxY = maxY
     }
 
     public boolean isPositionValid(Position position) {
--- tondeuse/src/main/java/com/tondeuse/Position.java
+++ tondeuse/src/main/java/com/tondeuse/Position.java
@@ -2,11 +2,30 @@ package com.tondeuse;
 
 public class Position {
 
-    int x;
-    int y;
+    private int x;
+    private int y;
 
     Position(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    // getters
+    public int getX() {
+        return x;
+
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    // setters
+    public int setX(int x) {
+        this.x = x;
+    }
+
+    public int getY( int y) {
+        this.y = y
     }
 }
